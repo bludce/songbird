195a0cea0dd7755d4e5c9fa834c565f0fff8035d
Aleksey Zadorozhny <alekszadorozhny90@gmail.com>
2020-02-10 11:33:01 +0300

    init: configure webpack

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.sass'
+
+function App() {
+  return (
+    <div className="red-text">Hello Ann</div>
+  );
+}
+
+ReactDOM.render((
+  <App/>
+), document.getElementById('root'));
